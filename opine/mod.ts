41f9b006eb80f578554a44292ce64d55f9cc3fd6
Tyler Laceby <31893030+tylerbadger23@users.noreply.github.com>
2022-10-19 09:48:57 -0600

    cleanup & implimentation of better header parsing.

--- opine/mod.ts
+++ opine/mod.ts
@@ -1,46 +1,103 @@
-/// <reference types="./opine-types.d.ts" />
-import { MultipartFormData } from "https://deno.land/x/denjucks@1.1.1/src/deps/path/std/mime/multipart.ts";
+import { MultipartReader } from "https://deno.land/x/denjucks@1.1.1/src/deps/path/std/mime/multipart.ts";
 
 import {
-  NextFunction,
-  OpineRequest,
-  OpineResponse,
+	NextFunction,
+	OpineRequest,
+	OpineResponse,
 } from "https://deno.land/x/opine@2.2.0/mod.ts";
 
-export default function multipartFormParser() {
-  return multipartParser;
+const DEFAULT_MAXIMUM_MEMORY = 1048576;
+
+export interface MultipartFormData {
+	file(key: string): FormFile | undefined;
+	value(key: string): string | undefined;
+	entries(): IterableIterator<[string, string | FormFile | undefined]>;
+	[Symbol.iterator](): IterableIterator<
+		[string, string | FormFile | undefined]
+	>;
+	/** Remove all tempfiles */
+	removeAll(): Promise<void>;
 }
 
-import {
-  MultipartReader,
-} from "https://deno.land/x/denjucks@1.1.1/src/deps/path/std/mime/mod.ts";
-
-async function multipartParser(
-  req: OpineRequest,
-  _res: OpineResponse,
-  next: NextFunction,
-) {
-  // Check the header for Content-Type
-  if (req.headers.has("Content-Type") == false) {
-    return next();
-  }
-
-  // Check for multipart header.
-  if (
-    (req.headers.get("Content-Type") as string).includes(
-      "multipart/form-data",
-    ) == false
-  ) {
-    return next();
-  }
-
-  const boundary = req.headers.get("Content-Type")?.split(";")[1].split(
-    "=",
-  )[1] as string;
-
-  const form_data = await new MultipartReader(req.body, boundary).readForm();
-  req.multipartData = form_data;
-  return next();
+export interface FormFile {
+	/** filename  */
+	filename: string;
+	/** content-type header value of file */
+	type: string;
+	/** byte size of file */
+	size: number;
+	/** in-memory content of file. Either content or tempfile is set  */
+	content?: Uint8Array;
+	/** temporal file path.
+	 * Set if file size is bigger than specified max-memory size at reading form
+	 * */
+	tempfile?: string;
 }
 
-export type MultipartData = MultipartFormData;
+// Handle OpineRequest override.
+declare module "https://deno.land/x/opine@2.2.0/mod.ts" {
+	interface OpineRequest {
+		multipartData?: MultipartFormData;
+		files?: FormFile[];
+	}
+}
+
+/**
+ * Optional configuration for multipart parsing middleware for opine.
+ */
+export interface MultipartOptions {
+	// Maximum memory to hold files before writing bytes to disk in temporary files.
+	// Default value is 1048576 bytes - 1MB
+	maxMemory?: number;
+
+	// Define whether to populate req.files with FormFile[]
+	// Default value is false.
+	files?: boolean;
+
+	// Whether to discard other formdata and populate the req.files field ONLY.\
+	// Default value is false.
+	filesOnly?: boolean;
+}
+
+export default function multipartFormParser(options?: MultipartOptions) {
+	// Validate configuration
+	options ??= {
+		maxMemory: DEFAULT_MAXIMUM_MEMORY,
+		files: false,
+		filesOnly: false,
+	};
+
+	// Set optional values
+	options.files ??= false;
+	options.filesOnly ??= false;
+	options.maxMemory ??= DEFAULT_MAXIMUM_MEMORY;
+
+	// Return middleware
+	return async (req: OpineRequest, _res: OpineResponse, next: NextFunction) => {
+		// Validate proper content-type is passed into headers.
+		const boundary = get_boundary(req);
+		if (!boundary) return next();
+
+		const form_data = await new MultipartReader(req.body, boundary).readForm();
+		req.multipartData = form_data;
+		return next();
+	};
+}
+
+function get_boundary(req: OpineRequest): null | string {
+	// Check the header for Content-Type
+	const contentType = req.headers.get("Content-Type");
+	if (contentType == null) {
+		return null;
+	}
+
+	if (contentType.includes("multipart/form-data") == false) return null;
+
+	// Parse out boundary string from header.
+	const header_rhs = contentType.split(";")[1];
+	if (header_rhs.includes("=")) {
+		return header_rhs.split("=")[1];
+	}
+
+	return null;
+}
